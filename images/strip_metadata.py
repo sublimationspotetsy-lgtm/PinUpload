a8cc436b6926185d3051cd910861ddd878706789
sublimationspotetsy-lgtm <sublimationspotetsy@gmail.com>
2026-08-25 12:39:53 +0100

    Push pin images from app

---
+++ images/strip_metadata.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+PNG Metadata Stripper - Main CLI Entry Point.
+"""
+
+import sys
+from png_meta_strip.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main())
